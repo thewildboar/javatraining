511e0de2873c1339d59b205bd9e3b549275bee15
Gianco <giancobox@gmail.com>
2016-02-14 17:17:30 +0100

    CIAO

---
+++ src/com/gianco/javatraining/JavaTraining.java
@@ -0,0 +1,10 @@
+package com.gianco.javatraining;
+
+/**
+ * Created by gianco on 14-Feb-16.
+ */
+public class JavaTraining {
+    public static void main(String[] args) {
+        System.out.println("CIAO");
+    }
+}
